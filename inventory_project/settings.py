e9eeaca8d0bc464cffac424bcf05110eed2f8580
sobitneupane <073bct543@ioe.edu.np>
2020-07-06 18:30:46 +0545

    changed whitenoise configuration

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'), )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
